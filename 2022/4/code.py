92ea94bb2af23f862e2e664fa592e4f10c32c88d
Erik <erik.bergrahm@live.com>
2022-12-11 21:17:52 +0100

    cleanup

--- 2022/4/code.py
+++ 2022/4/code.py
@@ -5,6 +5,14 @@
 part1_overlap = 0
 part2_intersect = 0
 
+def input(filename):
+    with open(filename, 'r') as input_file:
+        data = [ [ a, b, c, d ] for ((a, b), (c, d)) in [(tuple(a.split('-')), tuple(b.split('-'))) for (a, b) in [ line.strip().split(",") for line in input_file.readlines() ] ] ]
+    return data
+
+data = input("test.txt")
+print(data)
+
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
     for line in input.splitlines():
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # advent_of_code_2022
-My solutions to advent of code 2022. Using Alexe Simon's python script: [init.py](init.py) to setup project template.
+My solutions to advent of code 2022.
